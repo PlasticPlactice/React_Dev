680e3c3fe628b277efec86a51e07b27930480fea
Ryunosuke <r.kamata.sys23@morijyobi.ac.jp>
2024-11-13 11:23:50 +0900

    ユーザー情報の全件取得とユーザーの追加を作成 close #8, #9

---
+++ backend/database/users/users_add.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'db_connect.php';
+
+// ユーザーの追加
+if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    
+    $sql = "INSERT INTO car (name, email, password) VALUES(:name, :email, :password)";
+
+    $stm = $pdo->prepare($sql);
+
+    $stm->bindValue(':name', $name, PDO::PARAM_STR);
+    $stm->bindValue(':email', $email, PDO::PARAM_STR);
+    $stm->bindValue(':password', $password, PDO::PARAM_STR);
+    // SQLの実行
+    $stm->execute();
+}
+?>
---
+++ backend/database/users/users_all.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'db_connect.php';
+
+// ユーザー情報の全件取得
+$sql = "SELECT * FROM users";
+$stm = $pdo->prepare($sql);
+// SQLを実行
+$stm->execute();
+
+// ユーザー情報の全件取得
+$result = $stm->fetchAll(PDO::FETCH_ASSOC);
+foreach($result as $res){
+    echo 'ID:'.$res['id'].'name'.$res['name'].'email'.$res['email']."<br>";
+}
+?>
