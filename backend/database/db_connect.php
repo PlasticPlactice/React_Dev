c50332d76adb53ae98e6763fa943b893dbd22fda
yutohatano <y.hatano.sys23@morijyobi.ac.jp>
2024-11-26 09:51:53 +0900

    db_connectの修正と動的ルーティングの元になる機能を作成

--- backend/database/db_connect.php
+++ backend/database/db_connect.php
@@ -6,14 +6,14 @@
 //ユーザー名
 $user = "root";
 //パスワード
-$pass = "";
+$pass = "morijyobi";
 //データベース名
 $database = "car";
 //サーバー名
-$server = "localhost:3308";
+$server = "mysql";
 
 //接続するための情報を組み立てる（DSN文字列）
-$dsn = "mysql:host={$server};dbname={$database};charaset=utf8";
+$dsn = "mysql:host={$server};dbname={$database};charset=utf8";
 
 //PDOを使ってmysqlへ接続する
 try {
@@ -23,7 +23,7 @@ try {
     $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     //例外をスローする
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "データベースに接続しました";
+    echo "データベースに接続しました";
 } catch(Exception $e) {
     echo "データベース接続エラー";
     echo $e->getMessage();
