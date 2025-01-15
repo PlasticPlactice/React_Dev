c1ab3e477730006ad2f31845675e8095d6f16a6b
sachika <s.tsukeyanagi.sys23@morijyobi.ac.jp>
2025-01-15 11:40:08 +0900

    ボディータイプ検索を作成

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,6 +2,7 @@ import Footer from "@/components/Footer";
 import Search from "@/components/search";
 import History from "@/components/history";
 import Header from "@/components/Header";
+import Bodytypesearch from "@/components/Bodytypesearch";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
     <Header/>
     <Search/>
     <History/>
+    <Bodytypesearch/>
     <Footer/>
     </>
   
